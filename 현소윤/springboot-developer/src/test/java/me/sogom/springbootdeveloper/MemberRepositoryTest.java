9cbc10785bbb22f20c67f2cce379a6c8fd8eb778
soyoon0309 <soyoon0309@naver.com>
2025-10-05 03:00:00 +0900

    Test: Save method use
    save(member) : 1 entity
    saveAll(member) : 여러 entities를 동시에 저장
    현재 코드 상에서는 B와 C가 저장
    기존에 추가된 sql문이 있으면 안되는 관계로 @Sql 주석처리

--- 현소윤/springboot-developer/src/test/java/me/sogom/springbootdeveloper/MemberRepositoryTest.java
+++ 현소윤/springboot-developer/src/test/java/me/sogom/springbootdeveloper/MemberRepositoryTest.java
@@ -14,13 +14,30 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Sql("/insert-members.sql")
+//    @Sql("/insert-members.sql")
     @Test
-    void getMemberByName() {
+    /*void getMemberByName() {
         //when
         Member member = memberRepository.findByName("C").get();
 
         //then
         assertThat(member.getId()).isEqualTo(3);
+    }*/
+    /*void saveMember() {
+        //given
+        Member member = new Member(1L, "A");
+        //when
+        memberRepository.save(member);
+        //then
+        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+    }*/
+    void saveMembers() {
+        //given
+        List<Member> members = List.of(new Member(2L, "B"),
+                new Member(3L, "C"));
+        //when
+        memberRepository.saveAll(members);
+        //then
+        assertThat(memberRepository.findAll().size()).isEqualTo(2);
     }
 }
