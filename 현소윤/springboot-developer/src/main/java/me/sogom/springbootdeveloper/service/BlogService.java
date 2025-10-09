b33c4c7f9733cd4ba90936b81c07ec5606ade951
soyoon0309 <soyoon0309@naver.com>
2025-10-09 23:54:32 +0900

    Feat: 블로그 글 작성을 위한 API 구현

---
+++ 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,18 @@
+package me.sogom.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.sogom.springbootdeveloper.domain.Article;
+import me.sogom.springbootdeveloper.dto.AddArticleRequest;
+import me.sogom.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor    //final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service
+public class BlogService {
+    private final BlogRepository blogRepository;
+
+    //blog 글 추가 메서드
+    public Article save(AddArticleRequest request) {    //AddArticleRequest class에 저장된 값을 article DB에 저장
+        return blogRepository.save(request.toEntity());
+    }
+}
