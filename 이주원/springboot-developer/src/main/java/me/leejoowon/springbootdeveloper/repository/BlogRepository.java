8ce9feb020caf3922d9b325d2db465f9e570bf6d
이주원 <165635898+EricaLeeJoowon@users.noreply.github.com>
2025-10-14 03:10:09 +0900

    [이주원] 2025 GDG Spring Novice Study - 3주차 (#22)
    * [이주원] 2025 GDG Spring Novice Study - 1주차
    
    * [이주원] 2025 GDG Spring Novice Study - 1주차
    
    * [이주원] 2025 GDG Spring Novice Study - 1주차
    
    * Move : .idea 파일 이동
    
    * Move : .idea 파일 이동
    
    * Move : .idea 파일 이동
    
    * Move : .idea 파일 이동
    
    * Chore : .gitignore 수정
    
    * Chore: save local changes before pull
    
    * Feat: add springboot-developer project files
    
    * Chore: remove .DS_Store from repo
    
    * Fix: add springboot-developer files as normal directory
    
    * Feat: add springboot-developer source files
    
    * Fix: add springboot-developer project files as normal directory
    
    * Chore: 불필요한 파일 삭제
    
    * Chore : .gitignore 수정
    
    * Chore : .gitignore 수정
    
    * Chore : .gradle and build/ 파일 git에서 삭제
    
    * Chore : 2주차 실습 업로드
    
    * Chore : 2주차 실습 업로드
    
    * Chore : 2주차 실습 업로드
    
    * Chore : .Ds_Store 파일 제거
    
    * Chore : .gitignore 수정
    
    * Chore : .idea 삭제
    
    * Fix : build.gradle 수정
    
    * Fix : build.gradle
    
    * .gitignore and build.gradle test
    
    * Fix : update build.gradle and .gitignore
    
    * Chore : 3주차 업로드
    
    * Revert "Merge branch 'main' into main"
    
    This reverts commit 3ddbe33484e2d2ebe167e5e0219033e98ffac5dc, reversing
    changes made to b52fadfc3d611ef0011392b86c8540cbdb007a7a.

--- 이주원/springboot-developer/src/main/java/me/leejoowon/springbootdeveloper/repository/BlogRepository.java
+++ 이주원/springboot-developer/src/main/java/me/leejoowon/springbootdeveloper/repository/BlogRepository.java
@@ -1,4 +1,9 @@
 package me.leejoowon.springbootdeveloper.repository;
 
+import me.leejoowon.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
 public class BlogRepository {
 }
