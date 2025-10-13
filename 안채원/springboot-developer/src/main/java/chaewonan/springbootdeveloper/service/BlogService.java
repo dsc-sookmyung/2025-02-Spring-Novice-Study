43c8867ed23ebc3f5ed7abbf71dfb0af1b4bc524
anchaewon <anchaewon327@naver.com>
2025-10-13 22:23:03 +0900

    feat: add findAll() method in BlogService.java

--- 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/service/BlogService.java
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/service/BlogService.java
@@ -6,11 +6,16 @@ import chaewonan.springbootdeveloper.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
     private final BlogRepository blogRepository;
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
