482e1082fd5830e0ec1bfc1af9c820920dd7aad9
anchaewon <128976309+anchaewon@users.noreply.github.com>
2025-10-14 03:15:57 +0900

    [안채원] 2025 GDG Spring Novice Study - 3주차 (#25)
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * Delete .idea directory
    
    * chore: add .idea/ to .gitignore
    
    * chore: add .idea/ to .gitignore
    
    * feat: add TestController
    
    * Delete 안채원/.idea directory
    
    * chore: add application.yml
    
    * chore: update .idea
    
    * chore: update dependencies in build.gradle
    
    * feat: update TestController.java
    
    * feat: add TestService.java
    
    * feat: add Member.java
    
    * feat: add MemberRepository.java
    
    * fix: typo in build.gradle
    
    * test: add data.sql and update application.yml
    
    * test: MemberRepositoryTest
    
    * chore: update build.gradle
    
    * Delete .idea directory
    
    * chore: add .idea/ to .gitignore
    
    * chore: clean up files
    
    * feat: add Article.java
    
    * refactor: use lombok
    
    * feat: add BlogRepository.java
    
    * feat: add AddArticleRequest.java dto
    
    * feat: add BlogService.java
    
    * feat: add BlogApiController.java
    
    * chore: update application.yml
    
    * chore: remove data.sql
    
    * test: add BlogApiControllerTest.java
    
    * test: addArticle API test
    
    * feat: add findAll() method in BlogService.java
    
    * feat: add ArticleResponse.java
    
    * feat: add findAllArticles method in BlogApiController.java
    
    * test: add data.sql
    
    * test: findArticle API test
    
    * feat: add delete() method in BlogService.java
    
    * feat: add deleteArticles method in BlogApiController.java
    
    * test: findArticle API test
    
    * test: deleteArticle API test
    
    * feat: add update method in Article.java
    
    * feat: add UpdateArticleRequest.java
    
    * feat: add update() method in BlogService.java
    
    * feat: add updateArticle method in BlogApiController.java
    
    * test: updateArticle API test

--- 안채원/springboot-developer/src/main/resources/data.sql
+++ 안채원/springboot-developer/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO member (id, name) VALUES (1, 'chaewon')
-INSERT INTO member (id, name) VALUES (2, 'chaewonan')
-INSERT INTO member (id, name) VALUES (3, 'anchaewon')
+INSERT INTO article (title, content) VALUES ('제목 1', '내용 1')
+INSERT INTO article (title, content) VALUES ('제목 2', '내용 2')
+INSERT INTO article (title, content) VALUES ('제목 3', '내용 3')
