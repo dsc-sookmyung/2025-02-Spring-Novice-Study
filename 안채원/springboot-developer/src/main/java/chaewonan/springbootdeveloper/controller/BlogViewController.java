8d71865e4f812a6120e768f1edc5b6fc26f451cb
anchaewon <anchaewon327@naver.com>
2025-10-26 18:43:15 +0900

    feat: add getArticle() to BlogViewController.java

--- 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/controller/BlogViewController.java
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package chaewonan.springbootdeveloper.controller;
 
+import chaewonan.springbootdeveloper.domain.Article;
 import chaewonan.springbootdeveloper.dto.ArticleListViewResponse;
+import chaewonan.springbootdeveloper.dto.ArticleViewResponse;
 import chaewonan.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -15,13 +18,10 @@ public class BlogViewController {
 
     private final BlogService blogService;
 
-    @GetMapping("/articles")
-    public String getArticles(Model model) {
-        List<ArticleListViewResponse> articles = blogService.findAll().stream()
-                .map(ArticleListViewResponse::new)
-                .toList();
-        model.addAttribute("articles", articles);
-
-        return "articleList";
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+        return "article";
     }
 }
