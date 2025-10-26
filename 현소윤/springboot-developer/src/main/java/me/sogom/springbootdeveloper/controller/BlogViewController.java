a856d43711dafb53ccbf09c861d5292038c91744
soyoon0309 <soyoon0309@naver.com>
2025-10-26 22:33:37 +0900

    Feat: 블로그 글 뷰 구현
    Article.java : entity에 생성시간, 수정시간 추가
    DTO : 새로 생성
    Controller : id가 인자값으로 들어오는 경우 GetMapping 메서드 추가
    Application : 생성시간, 수정시간 자동 업데이트를 위해 JpaAuditing enable
    article.html 생성하여 테스트
    테스트를 위해 articleList.html 수정

--- 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/controller/BlogViewController.java
+++ 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.sogom.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.sogom.springbootdeveloper.domain.Article;
 import me.sogom.springbootdeveloper.dto.ArticleListViewResponse;
+import me.sogom.springbootdeveloper.dto.ArticleViewResponse;
 import me.sogom.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -22,5 +25,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);   //블로그 글 저장
 
         return "articleList";   //articleList.html view 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
--- 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/domain/Article.java
+++ 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/domain/Article.java
@@ -5,7 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity //엔티티로 지정
 @Getter //클래스 필드에 대해 별도 코드 없이 모든 필드에 대한 접근자 메서드 생성 가능
 @NoArgsConstructor(access = AccessLevel.PROTECTED)  //접근 제어자가 protected인 기본 생성자 대치
@@ -32,4 +38,12 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate    //엔티티가 생성될 때 생성 시간 저장
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate   //엔티티가 수정될 때 수정된 시간 저장
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
