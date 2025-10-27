e7beefc304af1f4c199991ba041ad4fb76c0800c
soyoon0309 <soyoon0309@naver.com>
2025-10-27 10:32:05 +0900

    Feat: logout 기능 구현
    GetMapping 이용해서 logout을 처리하도록 하고 articleList에 버튼을 추가함. 그리고 js 파일을 추가하여 동작할 수 있게 함.

--- 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/controller/UserApiController.java
+++ 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/controller/UserApiController.java
@@ -1,18 +1,31 @@
 package me.sogom.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import me.sogom.springbootdeveloper.dto.AddUserRequest;
 import me.sogom.springbootdeveloper.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class UserApiController {
     private final UserService userService;
+
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request); //회원가입 메서드 호출
         return "redirect:/login";   //회원가입 완료 후 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                new SecurityContextHolder().getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
