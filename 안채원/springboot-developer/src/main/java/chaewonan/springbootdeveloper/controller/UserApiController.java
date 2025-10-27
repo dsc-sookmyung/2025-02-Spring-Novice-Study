94ed7505e586477fbda824760ea20d4d4ed0419d
anchaewon <anchaewon327@naver.com>
2025-10-27 17:29:14 +0900

    feat: add logout endpoint to UserApiController.java

--- 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/controller/UserApiController.java
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/controller/UserApiController.java
@@ -2,8 +2,14 @@ package chaewonan.springbootdeveloper.controller;
 
 import chaewonan.springbootdeveloper.dto.AddUserRequest;
 import chaewonan.springbootdeveloper.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +21,12 @@ public class UserApiController {
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
