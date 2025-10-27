b54a593f04528806b2b18b29fd0ede1cc19502b0
ChaeYeon Lee <ichaeyeon090@gmail.com>
2025-10-27 20:03:50 +0900

    [이채연] 2025 GDG Spring Novice Study - 3주차 (#29)
    * Feat: 스프링 부트 3 프로젝트 만들기
    
    * Move: 파일 구조 변경
    
    * Move: springboot-developer 파일 main으로 이동
    
    * Move: springboot-developer main에 추가
    
    * Move: .idea 파일 삭제
    
    * Feat: /test GET 요청으로 문자열을 반환하는 TestController 추가
    
    * Build: build.gradle 의존성 추가
    
    * Feat: findAll() 메서드로 멤버 목록을 얻는 기능 추가
    
    * Refactor: TestController 클래스 추가 리팩터링
    
    * Feat: data.sql 생성
    
    * Test: 데이터를 잘 가져오는지 검증
    
    * Feat: id가 2인 멤버 찾기
    
    * Feat: name을 찾아주는 findByName 추가
    
    * Feat: 새로운 1번 멤버 'A' 추가
    
    * Test: 추가한 멤버 객체 수만큼 데이터에 있는지 확인
    
    * Test: id가 2인 멤버 삭제
    
    * Test: 모든 데이터 삭제
    
    * Test: cleanUp() 메서드로 삭제
    
    * Test: id가 2인 멤버의 이름을 'BC'로 바꾸기
    
    * Build: 새로운 프로젝트를 시작하기 위한 환경 세팅
    
    * Feat: 롬복을 사용하기 전 엔티티 구성
    
    * Feat: 롬복을 사용한 후 엔티티 구성
    
    * Move: 동일 파일 삭제
    
    * Feat: 리포지터리 생성
    
    * Feat: DTO를 엔티티로 만들어주는 메서드 추가
    
    * Feat: BlogService 클래스 구현
    
    * Feat: 컨트롤러 메서드 코드 작성
    
    * Test: API 실행 테스트하기
    
    * Test: 블로그 글 생성 API 테스트
    
    * Feat: finfAll() 메서드 추가
    
    * Feat: 전체 글 목록 조회하고 응답하는 역할 추가
    
    * Feat: 전체 글을 조회한 뒤 반환하는 기능 추가
    
    * Test: 블로그 글 목록 조회
    
    * Feat: 글 하나 조회하는 메서드 추가
    
    * Feat: 블로그 글을 조회하기 위한 매핑 메서드 추가
    
    * Test: 블로그 글 조회
    
    * Feat: 데이터베이스에서 데이터를 삭제하는 서비스 메서드 코드 추가
    
    * Feat: 글을 삭제하기 위한 컨트롤러 메서드 코드 작성
    
    * Test: 블로그 글 삭제
    
    * Feat: 엔티티에 요청받은 내용으로 값을 수정하는 메서드와  DTO 작성
    
    * Feat: 글을 수정하는 update() 메서드 추가
    
    * 글을 수정하기 위한 컨트롤러 메서드 코드 작성
    
    * Test: 블로그 글 수정

---
+++ LEECHAEYEON/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,46 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
+import me.shinsunyoung.springbootdeveloper.dto.UpdateArticleRequest;
+import me.shinsunyoung.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor //final이 붙거나 @Notnull이 붙은 필드의 생성자 추가
+@Service //빈으로 등록
+
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    //블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found" + id));
+    }
+
+    public void delete (long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found" + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
