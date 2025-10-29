78bcc63383935b51838252dd82ee8fa1293976dc
chaeyeon613 <ichaeyeon090@gmail.com>
2025-10-29 13:48:24 +0900

    feat: 수정 화면을 보여주기 위한 컨트롤러 메서드 추가

--- LEECHAEYEON/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
+++ LEECHAEYEON/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,10 +30,23 @@ public class BlogViewController {
     }
 
     @GetMapping("article/{id}")
-    public String getArticle(Model model, @PathVariable Long id) {
+    public String getArticle(@PathVariable("id") Long id, Model model) {
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    //id 키를 가진 쿼리 파라미터 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id,Model model) {
+        if(id == null) { //id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else { //id가 없으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
