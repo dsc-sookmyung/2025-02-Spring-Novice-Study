93f249d1c097c0cd9d1bb10c41e9b36afd08280c
ChaeYeon Lee <ichaeyeon090@gmail.com>
2025-10-30 20:58:01 +0900

    Delete 현소윤/springboot-developer directory

--- 현소윤/springboot-developer/src/main/java/me/sogom/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.sogom.springbootdeveloper.repository;
-
-import me.sogom.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
