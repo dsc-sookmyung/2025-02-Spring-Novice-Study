f358f8876f314625663401c5d011fad3474baccc
anchaewon <anchaewon327@naver.com>
2025-11-02 16:14:42 +0900

    merge upstream updates

--- 이주원/springboot-developer/src/main/java/me/leejoowon/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package me.leejoowon.springbootdeveloper.repository;
-
-import me.leejoowon.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
-public class BlogRepository {
-}
