ba564c60a2d99191301214a9a7d6b921b192d2a5
anchaewon <anchaewon327@naver.com>
2025-11-02 16:42:54 +0900

    feat: add JwtProperties.java

---
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/config/jwt/JwtProperties.java
@@ -0,0 +1,13 @@
+package chaewonan.springbootdeveloper.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Setter
+@Getter
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+}
