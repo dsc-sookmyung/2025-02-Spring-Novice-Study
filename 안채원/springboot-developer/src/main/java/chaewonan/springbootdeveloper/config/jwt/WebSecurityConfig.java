2d9c3a87b14a357f8ee971c9ce0cdca7d6fec3a3
anchaewon <anchaewon327@naver.com>
2025-11-03 18:13:26 +0900

    feat: add WebOAuthSecurityConfig.java

--- 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/config/jwt/WebSecurityConfig.java
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/config/jwt/WebSecurityConfig.java
@@ -17,50 +17,50 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class WebSecurityConfig {
-
-    private final UserDetailService userService;
-
-    @Bean
-    public WebSecurityCustomizer configure() {
-        return (web) -> web.ignoring()
-                .requestMatchers(toH2Console())
-                .requestMatchers(new AntPathRequestMatcher("/static/**"));
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests(auth -> auth
-                        .requestMatchers(
-                            new AntPathRequestMatcher("/login"),
-                            new AntPathRequestMatcher("/signup"),
-                            new AntPathRequestMatcher("/user")
-                        ).permitAll()
-                        .anyRequest().authenticated())
-                .formLogin(formLogin -> formLogin
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/articles")
-                )
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true)
-                )
-                .csrf(AbstractHttpConfigurer::disable)
-                .build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userService);
-        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
-        return new ProviderManager(authProvider);
-    }
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() { return new BCryptPasswordEncoder();}
-}
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class WebSecurityConfig {
+//
+//    private final UserDetailService userService;
+//
+//    @Bean
+//    public WebSecurityCustomizer configure() {
+//        return (web) -> web.ignoring()
+//                .requestMatchers(toH2Console())
+//                .requestMatchers(new AntPathRequestMatcher("/static/**"));
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeRequests(auth -> auth
+//                        .requestMatchers(
+//                            new AntPathRequestMatcher("/login"),
+//                            new AntPathRequestMatcher("/signup"),
+//                            new AntPathRequestMatcher("/user")
+//                        ).permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(formLogin -> formLogin
+//                        .loginPage("/login")
+//                        .defaultSuccessUrl("/articles")
+//                )
+//                .logout(logout -> logout
+//                        .logoutSuccessUrl("/login")
+//                        .invalidateHttpSession(true)
+//                )
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userService);
+//        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+//        return new ProviderManager(authProvider);
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder bCryptPasswordEncoder() { return new BCryptPasswordEncoder();}
+//}
