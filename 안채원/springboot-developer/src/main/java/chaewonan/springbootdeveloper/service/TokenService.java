3a6a338f6a95a5eb8dfe4447d3158ec475cb5d2a
anchaewon <anchaewon327@naver.com>
2025-11-03 21:09:06 +0900

    feat: update BlogApiControllerTest.java

--- 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/service/TokenService.java
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/service/TokenService.java
@@ -10,13 +10,12 @@ import java.time.Duration;
 @RequiredArgsConstructor
 @Service
 public class TokenService {
-
     private final TokenProvider tokenProvider;
     private final RefreshTokenService refreshTokenService;
     private final UserService userService;
 
     public String createNewAccessToken(String refreshToken) {
-        if (!tokenProvider.validToken(refreshToken)) {
+        if(!tokenProvider.validToken(refreshToken)) {
             throw new IllegalArgumentException("Unexpected token");
         }
 
