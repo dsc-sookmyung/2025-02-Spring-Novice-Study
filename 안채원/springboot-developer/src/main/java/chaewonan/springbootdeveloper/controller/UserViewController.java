d1dcd6fec3239433f98a74bda3fada897fc75ea4
anchaewon <anchaewon327@naver.com>
2025-11-03 18:41:51 +0900

    feat: update UserViewController.java

--- 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/controller/UserViewController.java
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/controller/UserViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserViewController {
     @GetMapping("/login")
-    public String login() { return "login"; }
+    public String login() { return "oauthlogin"; }
 
     @GetMapping("/signup")
     public String signup() { return "signup"; }
