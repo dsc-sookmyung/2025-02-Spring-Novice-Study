fb7d8283e1b7c97cefde1e3f5fd86a7aace5132d
이주원 <165635898+EricaLeeJoowon@users.noreply.github.com>
2025-11-05 10:50:47 +0900

    [이주원] 2025 GDG Spring Novice Study - 5주차 (#39)
    * [이주원] 2025 GDG Spring Novice Study - 1주차
    
    * [이주원] 2025 GDG Spring Novice Study - 1주차
    
    * [이주원] 2025 GDG Spring Novice Study - 1주차
    
    * Move : .idea 파일 이동
    
    * Move : .idea 파일 이동
    
    * Move : .idea 파일 이동
    
    * Move : .idea 파일 이동
    
    * Chore : .gitignore 수정
    
    * Chore: save local changes before pull
    
    * Feat: add springboot-developer project files
    
    * Chore: remove .DS_Store from repo
    
    * Fix: add springboot-developer files as normal directory
    
    * Feat: add springboot-developer source files
    
    * Fix: add springboot-developer project files as normal directory
    
    * Chore: 불필요한 파일 삭제
    
    * Chore : .gitignore 수정
    
    * Chore : .gitignore 수정
    
    * Chore : .gradle and build/ 파일 git에서 삭제
    
    * Chore : 2주차 실습 업로드
    
    * Chore : 2주차 실습 업로드
    
    * Chore : 2주차 실습 업로드
    
    * Chore : .Ds_Store 파일 제거
    
    * Chore : .gitignore 수정
    
    * Chore : .idea 삭제
    
    * Fix : build.gradle 수정
    
    * Fix : build.gradle
    
    * .gitignore and build.gradle test
    
    * Fix : update build.gradle and .gitignore
    
    * Chore : 3주차 업로드
    
    * Revert "Merge branch 'main' into main"
    
    This reverts commit 3ddbe33484e2d2ebe167e5e0219033e98ffac5dc, reversing
    changes made to b52fadfc3d611ef0011392b86c8540cbdb007a7a.
    
    * Chore : 4주차 실습 업로드
    
    * Chore : 4주차 실습 업로드
    
    * Resolve conflict: use main version of SpringBootDeveloperApplication.java
    
    * .gitignore에 application.yml 추가
    
    * Chore : 5주차 실습 업로드

--- 이주원/springboot-developer/src/main/java/me/leejoowon/springbootdeveloper/domain/Article.java
+++ 이주원/springboot-developer/src/main/java/me/leejoowon/springbootdeveloper/domain/Article.java
@@ -28,24 +28,25 @@ public class Article {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Builder
-    public Article(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
     @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @Column(name="author", nullable = false)
+    private String author;
 
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Builder
+    public Article(String author, String title, String content) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
-}
-public class Article {
 }
