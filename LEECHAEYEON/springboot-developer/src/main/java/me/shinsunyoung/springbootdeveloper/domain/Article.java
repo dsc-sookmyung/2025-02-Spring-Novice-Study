ffa2e5060776b24efe0d8944ef28ad11fb890661
chaeyeon613 <ichaeyeon090@gmail.com>
2025-11-05 19:35:45 +0900

    Feat: 로그인/로그아웃 구현

--- LEECHAEYEON/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
+++ LEECHAEYEON/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -16,20 +16,31 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
-
-    @Id //id 필드를 기본키로 지정
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "title", nullable = false) //'title'이라는 not null 컬럼과 매핑
+    @Column(name = "title", nullable = false)
     private String title;
 
     @Column(name = "content", nullable = false)
     private String content;
 
-    @Builder //빌더 패턴으로 객체 생성
-    public Article(String title, String content) {
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Builder
+    public Article(String author, String title, String content) {
+        this.author = author;
         this.title = title;
         this.content = content;
     }
@@ -38,13 +49,4 @@ public class Article {
         this.title = title;
         this.content = content;
     }
-
-    @CreatedDate
-    @Column(name="created_at")
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate //엔티티가 수정될 때 수정시간 저장
-    @Column(name="updated_at")
-    private LocalDateTime updatedAt;
-
 }
