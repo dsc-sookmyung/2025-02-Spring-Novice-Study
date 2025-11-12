fa20e767fe045247820f17b12ace223430d86090
binseoyun <sunnybin04@gmail.com>
2025-11-12 13:08:25 +0900

    5주차 과제 재업로드, application.yaml 수정

--- 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/SpringBootDeveloperApplication.java
+++ 안채원/springboot-developer/src/main/java/chaewonan/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -10,6 +10,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
 
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
