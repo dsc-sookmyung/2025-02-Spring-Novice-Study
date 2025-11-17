18e420b1c969671c042eca8a510c407d2d96342e
ChaeYeon Lee <ichaeyeon090@gmail.com>
2025-11-17 23:01:46 +0900

    [이채연] 2025 GDG Spring Novice Study - 5주차 (#56)
    * Feat: 스프링 부트 3 프로젝트 만들기
    
    * Move: 파일 구조 변경
    
    * Move: springboot-developer 파일 main으로 이동
    
    * Move: springboot-developer main에 추가
    
    * Move: .idea 파일 삭제
    
    * Feat: /test GET 요청으로 문자열을 반환하는 TestController 추가
    
    * Build: build.gradle 의존성 추가
    
    * Feat: findAll() 메서드로 멤버 목록을 얻는 기능 추가
    
    * Refactor: TestController 클래스 추가 리팩터링
    
    * Feat: data.sql 생성
    
    * Test: 데이터를 잘 가져오는지 검증
    
    * Feat: id가 2인 멤버 찾기
    
    * Feat: name을 찾아주는 findByName 추가
    
    * Feat: 새로운 1번 멤버 'A' 추가
    
    * Test: 추가한 멤버 객체 수만큼 데이터에 있는지 확인
    
    * Test: id가 2인 멤버 삭제
    
    * Test: 모든 데이터 삭제
    
    * Test: cleanUp() 메서드로 삭제
    
    * Test: id가 2인 멤버의 이름을 'BC'로 바꾸기
    
    * Build: 새로운 프로젝트를 시작하기 위한 환경 세팅
    
    * Feat: 롬복을 사용하기 전 엔티티 구성
    
    * Feat: 롬복을 사용한 후 엔티티 구성
    
    * Move: 동일 파일 삭제
    
    * Feat: 리포지터리 생성
    
    * Feat: DTO를 엔티티로 만들어주는 메서드 추가
    
    * Feat: BlogService 클래스 구현
    
    * Feat: 컨트롤러 메서드 코드 작성
    
    * Test: API 실행 테스트하기
    
    * Test: 블로그 글 생성 API 테스트
    
    * Feat: finfAll() 메서드 추가
    
    * Feat: 전체 글 목록 조회하고 응답하는 역할 추가
    
    * Feat: 전체 글을 조회한 뒤 반환하는 기능 추가
    
    * Test: 블로그 글 목록 조회
    
    * Feat: 글 하나 조회하는 메서드 추가
    
    * Feat: 블로그 글을 조회하기 위한 매핑 메서드 추가
    
    * Test: 블로그 글 조회
    
    * Feat: 데이터베이스에서 데이터를 삭제하는 서비스 메서드 코드 추가
    
    * Feat: 글을 삭제하기 위한 컨트롤러 메서드 코드 작성
    
    * Test: 블로그 글 삭제
    
    * Feat: 엔티티에 요청받은 내용으로 값을 수정하는 메서드와  DTO 작성
    
    * Feat: 글을 수정하는 update() 메서드 추가
    
    * 글을 수정하기 위한 컨트롤러 메서드 코드 작성
    
    * Test: 블로그 글 수정
    
    * Build: 타임리프 사용을 위한 의존성 추가
    
    * feat: 타임리프 문법 익히기용 컨트롤러 작성
    
    * feat: 뷰 작성
    
    * fix: model import 오류 해결
    
    * feat: 컨트롤러 메서드 작성
    
    * test: 블로그 글 목록 화면 테스트
    
    * feat: 엔티티에 생성, 수정 시간 추가
    
    * feat: 블로그 글 뷰를 위한 컨트롤러 메서드 작성
    
    * test: 블로그 글 뷰 확인
    
    * feat: 삭제 기능 코드 추가
    
    * feat: 수정 화면을 보여주기 위한 컨트롤러 메서드 추가
    
    * Feat: 수정/ 생성 뷰 만들기
    
    * Feat: 글 생성 기능 추가
    
    * Build: 스프링 시큐리티 의존성 추가
    
    * Fix: 타임리프가 보이지 않는 오류 해결
    
    * Feat: 회원 엔티티 생성
    
    * Feat: 레포지터리 생성
    
    * Feat: 시큐리티 설정
    
    * Feat: 사용자 정보를 가져오는 서비스 메서드 코드 작성
    
    * Feat: 사용자 정보를 가져오는 서비스 메서드 코드 작성
    
    * Feat: 회원 정보를 추가하는 서비스 메서드 작성
    
    * Feat: 로그인, 회원가입 뷰 컨트롤러 생성
    
    * Feat: 로그인, 회원가입 뷰 작성
    
    * Feat: 로그인, 로그아웃 페이지로 이동하는 컨트롤러 작성
    
    * Feat: 환경 변수 추가
    
    * Build: JWT 서비스를 구현하기 위한 의존성 추가
    
    * Build: 토큰 제공자 추가
    
    * Feat: 토큰 제공자 추가
    
    * Test: 토큰 제공자 테스트 코드 작성
    
    * Feat: 리프레시 토큰 도메인 구현
    
    * Feat: 토큰 필터 구현
    
    * Feat: 토큰 서비스 추가
    
    * Feat: 토큰 컨트롤러 추가
    
    * Test: 토큰 API 테스트 코드 작성
    
    * Build: 의존성 추가
    
    * Fix: application.yml 추적 제거
    
    * Fix: .env 파일 생성
    
    * Feat: 로그인/로그아웃 구현

--- LEECHAEYEON/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/dto/AddArticleRequest.java
+++ LEECHAEYEON/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/dto/AddArticleRequest.java
@@ -5,18 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.shinsunyoung.springbootdeveloper.domain.Article;
 
-@NoArgsConstructor //기본 생성자 추가
-@AllArgsConstructor //모든 필드 값을 파라미터로 받는 생성자 추가
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 public class AddArticleRequest {
-
     private String title;
     private String content;
-
-    public Article toEntity() { //생성자를 사용해 객체 생성
+    public Article toEntity(String author) {
         return Article.builder()
                 .title(title)
                 .content(content)
+                .author(author)
                 .build();
     }
 }
